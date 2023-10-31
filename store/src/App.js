f5a114021d07475ff19c499ee4dd98bb3301f759
anagiu <126169431+anagiu@users.noreply.github.com>
2023-10-31 17:19:18 -0300

    inicio

---
+++ store/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+          Online Shopping
+        </p>
+      
+          aaaaaaaaaa
+      </header>
+    </div>
+  );
+}
+
+export default App;
