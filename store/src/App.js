d53305cfaff8de237efcf400c41d49b3617cd6f4
Cherry <126169431+anagiu@users.noreply.github.com>
2023-11-07 02:13:15 -0300

    iniciando login

--- store/src/App.js
+++ store/src/App.js
@@ -1,8 +1,42 @@
 
+
+import './style.css'
+
 function App() {
   return (
-<h1>aaaaaaaaaaa</h1>
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
 
+            <span className="login-form-title">log in</span>
+
+            <div className="wrap-input">
+              <input className='input' type="email"/>
+              <span className="focus-input" data-placeholder="Email"></span>
+            </div>
+
+            <br></br>
+
+            <div className="wrap-input">
+              <input className="input" type="password" />
+              <span className="focus-input" data-placeholder="Password"></span>
+            </div>
+
+            <br></br>
+
+            <div className="container-login-form-btn"></div>
+            <button className="login-form-btn">login</button>
+
+            <div className="text-center">
+              <span className="txt1">Não possui conta?</span>
+              <a className="txt2" href="#">Criar conta.</a>
+            </div>
+
+          </form>
+      </div>
+    </div>
+</div>
   );
 }
 
